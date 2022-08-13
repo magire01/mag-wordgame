7f9f2ea530ebba2868046db1d5c5c595b5c8cd8e
magire01 <magire01@gmail.com>
2022-08-13 16:23:35 -0400

    Updated Words

--- WordGame/GameViewController.swift
+++ WordGame/GameViewController.swift
@@ -219,7 +219,7 @@ extension GameViewController: UITableViewDelegate, UITableViewDataSource, Handle
                 refreshRow()
                 completeGame()
             } else {
-                print("Not a word")
+                self.showToast(message: "Not A Word", font: .systemFont(ofSize: 12.0))
             }
         }
     }
@@ -359,3 +359,22 @@ extension GameViewController: UITableViewDelegate, UITableViewDataSource, Handle
     }
 }
 
+extension GameViewController {
+    func showToast(message : String, font: UIFont) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-400, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+}
